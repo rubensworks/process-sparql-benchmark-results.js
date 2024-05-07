92a9d9f33ca06fd4b2d764f7af288a1c93ce8823
Ruben Taelman <ruben.taelman@ugent.be>
2024-05-07 20:55:28 +0200

    Calculate averages in ghbench

--- lib/cli/commands/csv/CommandGithubActionsBenchmark.ts
+++ lib/cli/commands/csv/CommandGithubActionsBenchmark.ts
@@ -4,6 +4,7 @@ import type { Argv } from 'yargs';
 import { wrapCommandHandler, wrapVisualProgress } from '../../CliHelpers';
 import type { ITaskContext } from '../../ITaskContext';
 import {
+  calcAverage,
   getExperimentNames,
   handleCsvFile,
 } from '../tex/TexUtils';
@@ -57,25 +58,42 @@ export const handler = (argv: Record<string, any>): Promise<void> => wrapCommand
 
     // Collect timings
     const ghbenchData: GhbenchData = [];
+    const ghbenchDataRaw: Record<string, GhbenchDataRaw> = {};
     for (const [ experimentId, experimentDirectory ] of experimentDirectories.entries()) {
-      let total = 0;
-
       await handleCsvFile(experimentDirectory, argv, data => {
         if (!queryRegex || queryRegex.test(data.name)) {
-          // Add one to avoid zero values, as the action does not handle those well for threshold checking.
-          const value = Number.parseInt(data.results, 10) + 1;
-          total += value;
-          if (argv.detailed) {
-            ghbenchData.push({
-              name: `${experimentNames[experimentId]} - ${data.name}.${data.id}`,
+          const value = Number.parseInt(data.time, 10);
+          if (!ghbenchDataRaw[data.name]) {
+            ghbenchDataRaw[data.name] = {
+              name: `${experimentNames[experimentId]} - ${data.name}`,
               unit: 'ms',
-              value,
+              values: [ value ],
               extra: `Results: ${data.results}; Error: ${data.error}; HTTP Requests: ${data.httpRequests}`,
-            });
+            };
+          } else {
+            ghbenchDataRaw[data.name].values.push(value);
           }
         }
       });
 
+      // Calculate averages
+      let total = 0;
+      for (const entry of Object.values(ghbenchDataRaw)) {
+        const value = calcAverage(entry.values);
+        total += value;
+
+        // Output detailed
+        if (argv.detailed) {
+          ghbenchData.push({
+            name: entry.name,
+            unit: entry.unit,
+            value,
+            extra: entry.extra,
+          });
+        }
+      }
+
+      // Output totals
       if (argv.total) {
         ghbenchData.push({
           name: `${experimentNames[experimentId]}`,
@@ -89,6 +107,14 @@ export const handler = (argv: Record<string, any>): Promise<void> => wrapCommand
     // eslint-disable-next-line no-sync
     fs.writeFileSync(Path.join(context.cwd, `${argv.name}`), JSON.stringify(ghbenchData, null, '  '), 'utf8');
   }));
+
+interface GhbenchDataRaw {
+  name: string;
+  unit: string;
+  values: number[];
+  range?: string;
+  extra?: string;
+}
 
 type GhbenchData = {
   name: string;
